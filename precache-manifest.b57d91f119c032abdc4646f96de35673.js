36a617480582235abb187b75b087b641fc6224a2
IlyaErshovDev <yosh7595@gmail.com>
2020-10-21 16:58:47 +0300

    Updates

---
+++ precache-manifest.b57d91f119c032abdc4646f96de35673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b157eaf59fff3375e2f80a06877a2f",
+    "url": "/note-manager/index.html"
+  },
+  {
+    "revision": "de56d533a9d6231e0036",
+    "url": "/note-manager/static/css/main.360edcaa.chunk.css"
+  },
+  {
+    "revision": "b7ffa2b3efa3beea3054",
+    "url": "/note-manager/static/js/2.96513bf6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/note-manager/static/js/2.96513bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de56d533a9d6231e0036",
+    "url": "/note-manager/static/js/main.7951b812.chunk.js"
+  },
+  {
+    "revision": "f3ada56fcf31c9be7023",
+    "url": "/note-manager/static/js/runtime-main.45c67289.js"
+  }
+]);
